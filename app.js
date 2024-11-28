bdcd3c8e676c6f468e37902c14a6b888cab39838
kim nan seul <154604350+seulnan@users.noreply.github.com>
2024-11-28 23:07:26 +0900

    [FIX] cors오류
    cors미들웨어
    options요청에 cors 허용해서 올림

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const uploadRoutes = require('./routes/upload');
 
 const app = express();
 
+const cors = require('cors');
+
+// CORS 미들웨어 추가
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // 허용할 클라이언트 URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // 허용할 HTTP 메서드
+    credentials: true, // 인증 정보(쿠키 등) 허용 여부
+  })
+);
+
+app.options("*", cors()); // 모든 OPTIONS 요청에 CORS 허용
 // 미들웨어 설정
 app.use(express.json());
 
